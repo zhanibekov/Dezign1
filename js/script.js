0870914213a18273917a8d3fb0d059a12d0054a5
yelaman <67456840+zhanibekov@users.noreply.github.com>
2022-12-02 11:36:44 +0600

    In this commit, I added an anchor and added a glide script

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ if ( menuBtn && menu) {
 		})
 	})
 }
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', e => {
+		e.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+	})
+})
